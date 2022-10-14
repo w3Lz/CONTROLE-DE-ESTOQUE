60f40326778204ba1b9b9d245abdf6aefeef505c
w3Lz <welzwelz0@gmail.com>
2022-10-13 21:39:32 -0300

    refactor: add architeture

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const userRoute = require("./src/routes/user.route")
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+
+const app = express();
+
+app.use("/soma", userRoute);
+
+// ROTA
+  // METODOS HTTP - CRUD ( CREATE, READ, UPDATE, DELETE )
+    // GET    - PEGA UMA INFO
+    // POST   - CRIA UMA UNFO
+    // PUT    - ALTERA TODA A INFO
+    // PATH   - ALTERA PARTE DA INFO
+    // DELETE - APAGA UMA INFO
+
+  // Name - UM IDENTIFICADOR DA ROTA
+
+  // Function ( callback ) - RESPONSAVEL POR EXECUTAR ALGUM COMANDO
 
 app.listen(3000)
